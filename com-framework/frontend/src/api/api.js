232cc08ef9a897404de307e81b5f8c936f2d9f10
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-09-10 15:23:15 -0300

    fixed bugs

--- com-framework/frontend/src/api/api.js
+++ com-framework/frontend/src/api/api.js
@@ -1,9 +1,24 @@
 import axios from "axios";
-
-const {VITE_ENV} = import.meta.env; // não process.env
-// const { NODE_ENV } = process.env;
+const { VITE_ENV } = import.meta.env; // não process.env
 const url = VITE_ENV === "development" ? "http://localhost:3001/api" : "/api";
-const resArtists = await axios.get(`${url}/artists`);
-const resSongs = await axios.get(`${url}/songs`);
-export const artistArray = resArtists.data;
-export const songsArray = resSongs.data;
+let artistArray = [];
+let songsArray = [];
+
+try {
+  // const { NODE_ENV } = process.env;
+  const resArtists = await axios.get(`${url}/artists`);
+  const resSongs = await axios.get(`${url}/songs`);
+  artistArray = resArtists.data;
+  songsArray = resSongs.data;
+} catch (error) {
+  console.error("Erro ao buscar dados da API: ", error);
+  await axios
+    .post(`${url}/telemetria`, "Erro ao buscar dados da API: " + error)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+export {artistArray,songsArray};

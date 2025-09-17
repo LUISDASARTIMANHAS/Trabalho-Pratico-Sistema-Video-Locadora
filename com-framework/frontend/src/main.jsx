b496d2982b5f158dd85bb33759565cfcdce15ccd
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-09-17 16:22:39 -0300

    rework

--- com-framework/frontend/src/main.jsx
+++ com-framework/frontend/src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './global.css'
 import App from './App.jsx'
 
+// este e o core server não mexer nele se quiser adicionar algo faça em App.jsx ou crie um novo app e chame aqui
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
